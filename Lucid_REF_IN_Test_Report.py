434ee8945d893a1a9e7d90fc2d29136ec9876fb0
MihirGit2023 <mihir@taborelec.com>
2024-04-08 12:26:18 +0530

    Commit after modified of script after applying filter on channels so that output skew not much affectd and tested correctly.

--- Lucid_REF_IN_Test_Report.py
+++ Lucid_REF_IN_Test_Report.py
@@ -100,15 +100,11 @@ scope.write(':MEASure:CLEar')
 scope.write('*CLS;:DISPlay:CGRade:LEVels ')
 ##############################################################################################################################################
 
-# Initialization
-send_scpi_cmd('*CLS',handle)
-send_scpi_cmd('*RST',handle)
-send_scpi_cmd(':INST 1',handle)
-send_scpi_cmd(':INST:ACT 1',handle)
+
 
 frequency_list= [10e6,10e6,10e6,100e6,100e6,100e6]
 power_list= [-5,0,10,-5,0,10]
-
+measured_skew= []
 
 serial_no_tb = np.zeros(len(frequency_list)*len(power_list))
 sig_gen_freq_tb = np.zeros(len(frequency_list)*len(power_list))
@@ -116,113 +112,106 @@ sig_gen_power_tb = np.zeros(len(frequency_list)*len(power_list))
 output_status_tb= []
 table_i = 0
 
-ideal_min_width = [425e-09,25.5e-09]
-ideal_max_width = [575e-09,34.5e-09]
-ideal_min_period = [0.95e-06,0.95e-07]
-ideal_max_period = [1.05e-06,1.05e-07]
-timebase_scale = [500e-09,20e-09]
-timebase_position = [423.4E-9,423.4E-9]
+timebase_scale = [50e-09,50e-09,50e-09,10e-09,10e-09,10e-09]
+timebase_position = [0,0,0,0,0,0]
+channel1_scale = [300e-03,300e-03,300e-03,300e-03,300e-03,300e-03]
+channel2_scale = [100e-03,200e-03,500e-03,100e-03,200e-03,500e-03]
+test = 0
 
 
+sig_gen_freq_tb = list(frequency_list)
+sig_gen_power_tb = list(power_list)
 
 
+for test1 in range(0,6,3):
 
-for test in range(2):
+    # Initialization
+    send_scpi_cmd('*CLS', handle)
+    send_scpi_cmd('*RST', handle)
+    send_scpi_cmd(':INST 1', handle)
+    send_scpi_cmd(':INST:ACT 1', handle)
+
     # Set the cw to 500Mhz and Powe 10 dbm
-    send_scpi_cmd(':FREQuency {}'.format(frequency_list[table_i]),handle)
+    send_scpi_cmd(':FREQuency {}'.format(frequency_list[test1]),handle)
+    print('Selected frequency is {}'.format(frequency_list[test1]))
     send_scpi_cmd(':POWer 5',handle)
 
     send_scpi_cmd(':ROSC:SOUR EXT', handle)
-    send_scpi_cmd(':ROSC:SOUR:FREQ {}'.format(frequency_list[table_i]), handle)
-
-    scope.write(':CHANnel{}:PROBe 1.0'.format(1))
-    scope.write(':CHANnel{}:SCALe 200E-3'.format(1))
-    scope.write(':CHANnel{}:OFFSet 0.0'.format(1))
-    scope.write(':TRIGger:MODE EDGE')
-    scope.write(':TRIGger:EDGE:SOURce CHANnel{}'.format(3))
-    time.sleep(2)
-    scope.write(':TIMebase:SCALe {}'.format(timebase_scale[test]))
-    scope.write(':TIMebase:POSition {}'.format(timebase_position[test]))
-
+    send_scpi_cmd(':ROSC:SOUR:FREQ {}'.format(frequency_list[test1]), handle)
 
     send_scpi_cmd(':OUTPut ON', handle)
-
-
-
-    print('Please set the external signal generator frequency {} and power {} dBm and press enter'.format(frequency_list[table_i],power_list[table_i]))
+    print('Please set the external signal generator frequency {} and power {} dBm and press enter'.format(frequency_list[test1],power_list[table_i]))
     input()
-    scope.write('*CLS;:DISPlay:CGRade:LEVels ')
+    # scope.write('*CLS;:DISPlay:CGRade:LEVels ')
     scope.write('AUTOscale')
     time.sleep(2)
-    scope.write(':CHANnel{}:SCALe 400E-3'.format(1))
+    scope.write(':CHANnel{0}:SCALe {1}'.format(1,channel1_scale[test1]))
     scope.write(':CHANnel{}:OFFSet 0.0'.format(1))
-    scope.write(':CHANnel{}:SCALe 400E-3'.format(2))
+    scope.write(':CHANnel{0}:SCALe {1}'.format(2,channel2_scale[test1]))
     scope.write(':CHANnel{}:OFFSet 0.0'.format(2))
+    scope.write(':TIMebase:SCALe {}'.format(timebase_scale[test1]))
+    scope.write(':TIMebase:POSition {}'.format(timebase_position[test1]))
+    scope.write(':TRIGger:MODE EDGE')
+    scope.write(':TRIGger:EDGE:SOURce CHANnel{}'.format(1))
 
-    send_scpi_cmd(':PHAS 0', handle)
-    time.sleep(1)
-    scope.write(':MEASure:PHASe CHANnel1,CHANnel2')
+    time.sleep(2)
+
+    scope.write(':CHANnel1:ISIM:BWLimit:WALL')
+    scope.write(':CHANnel1:ISIM:BWLimit ON')
+    scope.write(':CHANnel1:ISIM:BANDwidth 200e6')
+
+    scope.write(':CHANnel2:ISIM:BWLimit:WALL')
+    scope.write(':CHANnel2:ISIM:BWLimit ON')
+    scope.write(':CHANnel2:ISIM:BANDwidth 200e6')
+
+
+
+    scope.write(':MEASure:DELTatime:DEFine RISing,1,MIDDle,RISing,1,MIDDle')
+    time.sleep(2)
+    scope.write(':MEASure:DELTatime CHANnel1,CHANnel2')
     time.sleep(2)
     scope.write(':MEASure:RESults?')
     result = scope.read()
-    measured_phase = float(result.split(',')[1])
-    print('Measured phase is {}'.format(measured_phase))
+    measured_skew_val = float(result.split(',')[1])
+    print('Measured skew is {}'.format(measured_skew_val))
 
-    if abs(measured_phase) < 0:
-        send_scpi_cmd(':PHAS {}'.format(abs(measured_phase)), handle)
-    else:
-        send_scpi_cmd(':PHAS {}'.format(360 - abs(measured_phase)), handle)
-
-    time.sleep(3)
-    scope.write(':MEASure:PHASe CHANnel1,CHANnel2')
-    time.sleep(2)
-    scope.write(':MEASure:RESults?')
-    result = scope.read()
-    measured_phase = float(result.split(',')[1])
-    print('Measured phase is {}'.format(measured_phase))
-
-    if abs(measured_phase) < 5:
-        print('Test Pass')
-        print('Phase locked')
-        print(table_i)
-        output_status_tb.append("Pass")
-    else:
-        print('Test Fail')
-        print('Phase not locked')
-        print(table_i)
-        output_status_tb.append("Fail")
 
     serial_no_tb[table_i] = table_i + 1
-    sig_gen_freq_tb[table_i]= frequency_list[table_i]
-    sig_gen_power_tb[table_i] = power_list[table_i]
     table_i = table_i + 1
+    measured_skew.append(measured_skew_val)
+    initial_skew = measured_skew_val
+    output_status_tb.append("Pass")
 
-    for in_siggenpow in range(2):
-        print('Change the signal generator power to {} dBm and press enter'.format(power_list[in_siggenpow + 1]))
+
+    for test in range(2):
+        print('Please set the external signal generator frequency {} and power {} dBm and press enter'.format(
+            frequency_list[test+test1+1], power_list[test+test1+1]))
         input()
+        scope.write('*CLS;:DISPlay:CGRade:LEVels ')
+        time.sleep(2)
+        scope.write(':CHANnel{0}:SCALe {1}'.format(2, channel2_scale[test+test1+1]))
+        scope.write(':CHANnel{}:OFFSet 0.0'.format(2))
+        time.sleep(1)
         scope.write(':MEASure:DELTatime:DEFine RISing,1,MIDDle,RISing,1,MIDDle')
         time.sleep(2)
         scope.write(':MEASure:DELTatime CHANnel1,CHANnel2')
         time.sleep(2)
         scope.write(':MEASure:RESults?')
         result = scope.read()
-        measured_skew = float(result.split(',')[1])
-        print('Measured skew is {}'.format(measured_skew))
+        measured_skew_val = float(result.split(',')[1])
+        print('Measured skew is {}'.format(measured_skew_val))
+        serial_no_tb[table_i] = table_i +1
+        measured_skew.append(measured_skew_val)
 
-        if measured_skew > -1.5e9 and measured_skew < 1.5e9:
-            print('Test Pass for power level {} dBm'.format(power_list[in_siggenpow + 1]))
+        tested_skew = abs(measured_skew[test+test1+1]) - abs(initial_skew)
+        if tested_skew > -1.5e-09 and tested_skew < 1.5e-09:
+            print('Test Pass')
             output_status_tb.append("Pass")
         else:
-            print('Test Fail for power level {} dBm'.format(power_list[in_siggenpow + 1]))
+            print('Test Fail')
             output_status_tb.append("Fail")
 
-        serial_no_tb[table_i] = table_i + 1
-        sig_gen_freq_tb[table_i] = frequency_list[table_i]
-        sig_gen_power_tb[table_i] = power_list[table_i]
         table_i = table_i + 1
-    print('Test completed for frequency {}'.format(frequency_list[test]))
-print('Test Completed')
-
 
 ###################################################################################################################################################
 
@@ -244,6 +233,90 @@ print("Table has been written to Reference Internal source test report.txt")
 
 print('Test Run successfully')
 
+
+    # scope.write(':MEASure:PHASe CHANnel1,CHANnel2')
+    # time.sleep(2)
+    # scope.write(':MEASure:RESults?')
+    # result = scope.read()
+    # measured_phase = float(result.split(',')[1])
+    # print('Measured phase is {}'.format(measured_phase))
+
+    # if abs(measured_phase) < 0:
+    #     send_scpi_cmd(':PHAS {}'.format(abs(measured_phase)), handle)
+    # else:
+    #     send_scpi_cmd(':PHAS {}'.format(360 - abs(measured_phase)), handle)
+
+    # time.sleep(3)
+    # scope.write(':MEASure:PHASe CHANnel1,CHANnel2')
+    # time.sleep(2)
+    # scope.write(':MEASure:RESults?')
+    # result = scope.read()
+    # measured_phase = float(result.split(',')[1])
+    # print('Measured phase is {}'.format(measured_phase))
+
+    # if abs(measured_phase) < 5:
+    #     print('Test Pass')
+    #     print('Phase locked')
+    #     print(table_i)
+    #     output_status_tb.append("Pass")
+    # else:
+    #     print('Test Fail')
+    #     print('Phase not locked')
+    #     print(table_i)
+    #     output_status_tb.append("Fail")
+
+#     serial_no_tb[table_i] = table_i + 1
+#     sig_gen_freq_tb[table_i]= frequency_list[table_i]
+#     sig_gen_power_tb[table_i] = power_list[table_i]
+#     table_i = table_i + 1
+#
+#     for in_siggenpow in range(2):
+#         print('Change the signal generator power to {} dBm and press enter'.format(power_list[in_siggenpow + 1]))
+#         input()
+#         scope.write(':MEASure:DELTatime:DEFine RISing,1,MIDDle,RISing,1,MIDDle')
+#         time.sleep(2)
+#         scope.write(':MEASure:DELTatime CHANnel1,CHANnel2')
+#         time.sleep(2)
+#         scope.write(':MEASure:RESults?')
+#         result = scope.read()
+#         measured_skew = float(result.split(',')[1])
+#         print('Measured skew is {}'.format(measured_skew))
+#
+#         if measured_skew > -1.5e9 and measured_skew < 1.5e9:
+#             print('Test Pass for power level {} dBm'.format(power_list[in_siggenpow + 1]))
+#             output_status_tb.append("Pass")
+#         else:
+#             print('Test Fail for power level {} dBm'.format(power_list[in_siggenpow + 1]))
+#             output_status_tb.append("Fail")
+#
+#         serial_no_tb[table_i] = table_i + 1
+#         sig_gen_freq_tb[table_i] = frequency_list[table_i]
+#         sig_gen_power_tb[table_i] = power_list[table_i]
+#         table_i = table_i + 1
+#     print('Test completed for frequency {}'.format(frequency_list[test]))
+# print('Test Completed')
+#
+#
+# ###################################################################################################################################################
+#
+#
+# # Sample data for the table
+# data = list(zip(serial_no_tb, sig_gen_freq_tb, sig_gen_power_tb, output_status_tb))
+#
+# # Creating the table
+# table = tabulate(data, headers=["Test step", "Signal Generator Frequency", "Signal Generator Power", "PASS/FAIL"], tablefmt="grid")
+#
+# # Displaying the table
+# print(table)
+#
+# # Writing the table to a text file
+# with open("Lucid_REF_IN_Test_Report.txt", "w") as f:
+#     f.write(table)
+#
+# print("Table has been written to Reference Internal source test report.txt")
+#
+# print('Test Run successfully')
+#
 
 
 
